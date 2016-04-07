b47a80c11dc0287fb3490a26fcf1ea2e2716fdbe
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-04-07 17:17:16 -0400

    add nested routes and registration views

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,6 @@
 class RegistrationsController < ApplicationController
+  def index
+    @piece = Piece.find(params[:piece_id])
+    @registrations = @piece.registrations
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :registrations
+  resources :pieces do
+    resources :registrations
+  end
   root 'static_pages#home'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-
-  resources :pieces
 end
