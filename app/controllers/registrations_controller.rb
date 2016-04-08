f25c10a26eec9b676ccce2741546ba348a5a9193
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-04-08 15:35:22 -0400

    implement registration destroy action

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -19,6 +19,13 @@ class RegistrationsController < ApplicationController
     end
   end
 
+  def destroy
+    @registration = @piece.registrations.find((params[:id]))
+    name = @registration.name
+    @registration.destroy
+    redirect_to piece_registrations_url, notice: "#{name}'s request was successfully removed!"
+  end
+
   private
 
   def registration_params
@@ -26,6 +33,6 @@ class RegistrationsController < ApplicationController
   end
 
   def set_piece
-    @piece = Piece.find(params[:event_id])
+    @piece = Piece.find(params[:piece_id])
   end
 end
