4a97eba48ef5d244215e562a59b633554089c31b
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-04-08 14:36:27 -0400

    clean up errors and registration controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,16 +1,15 @@
 class RegistrationsController < ApplicationController
+  before_action :set_piece
+
   def index
-    @piece = Piece.find(params[:piece_id])
     @registrations = @piece.registrations
   end
 
   def new
-    @piece = Piece.find(params[:piece_id])
     @registration = @piece.registrations.new
   end
 
   def create
-    @piece = Piece.find(params[:piece_id])
     @registration = @piece.registrations.new(registration_params)
     if @registration.save
       flash[:notice] = "Your registration for #{@piece.title} has been sent!"
@@ -24,5 +23,9 @@ class RegistrationsController < ApplicationController
 
   def registration_params
     params.require(:registration).permit(:name, :email, :area_code, :phone_number, :message, :interested_in_rental, :interested_in_purchase, :neighborhood)
+  end
+
+  def set_piece
+    @piece = Piece.find(params[:event_id])
   end
 end
