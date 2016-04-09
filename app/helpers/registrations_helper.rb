80612f34819ad15033315b6f72d807432b4f3c1e
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-04-08 20:01:11 -0400

    add helper messaging to registration index

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,2 +1,13 @@
 module RegistrationsHelper
+  def normalize_note_for(request)
+    if (request.interested_in_rental == true)  && (request.interested_in_purchase == true)
+      "#{request.name} is interested in both renting and purchasing this piece."
+    elsif (request.interested_in_rental == true) && (request.interested_in_purchase == false)
+      "#{request.name} is interested in renting this piece."
+    elsif (request.interested_in_rental == false) && (request.interested_in_purchase == true)
+      "#{request.name} is interested in purchasing this piece."
+    else
+      "#{request.name} did not indicate a preference for either rental or purchase."
+    end
+  end
 end
