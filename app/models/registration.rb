5b8f8b163d6d00e8d7ca573677cd43720fdc1b5f
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-04-10 13:08:59 -0400

    home about and contact page routed

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -2,11 +2,12 @@ class Registration < ActiveRecord::Base
   before_save { |registration| registration.email = registration.email.downcase }
   belongs_to :piece
 
-  validates :name, presence: true, length: { within: 1..25 }
+  validates :name, length: { within: 1..25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
-  validates :phone_number, presence: true, numericality: { only_integer: true }
+  validates :phone_number, numericality: { only_integer: true }, length: { is: 10, message: 'should be 10 digits starting with area code'}
   validates :message, presence: false
-  validates :zip_code, presence: true, numericality: { only_integer: true }
+  validates :neighborhood, presence: true
+  validates :zip_code, length: { is: 5, message: 'should be 5 digits' }
   validates :interested_in_rental, :interested_in_purchase, inclusion: { in: [true, false] }
 end
